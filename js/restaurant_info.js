4e74463d14df63e272c4d94c37dc47d04d2f40ba
Asmaa-Abou-Bakr <asmaa.aboBaker@yahoo.com>
2019-01-31 23:49:00 +0200

    fix map in internal pages

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYXNtYWEtYWJvYmFrZXIiLCJhIjoiY2pyamM5MGRoMGFhODQ0bWw0c3ViaXllaSJ9.G9oLnBI8xHZ7rJyJEvNZnQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
