489b49eeb85bc7ea9ace727055157bdbcbbc7ef0
Asmaa-Abou-Bakr <asmaa.aboBaker@yahoo.com>
2019-02-04 17:53:48 +0200

    caching the filles to work offline

--- js/main.js
+++ js/main.js
@@ -214,3 +214,5 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 } */
 
+
+
---
+++ sw.js
@@ -0,0 +1,49 @@
+let staticCacheName = 'rest-app-v1';
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('rest-app-v1').then(function(cache) {
+      return cache.addAll([
+        'js/main.js',
+        'js/dbhelper.js',
+		'js/reg.js',
+        'js/restaurant_info.js',
+        'css/style.css',
+        'css/custom.css',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg',
+        'img/10.jpg'        
+        ]);
+    })
+  );
+});
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys()
+        .then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                           cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+})
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
